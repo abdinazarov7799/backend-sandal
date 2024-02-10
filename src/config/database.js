815333042afdb465000bfb5076e31dd4ded8db49
abdinazarov7799 <parolxato@gmail.com>
2024-02-11 01:47:58 +0500

    .

--- src/config/database.js
+++ src/config/database.js
@@ -8,5 +8,4 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('postgres://sandal:JGX0INLAjYDjqD4N32TOsAdreqXDADaC@dpg-cn1mluol5elc73d9uc30-a/sandal_fm9t');
 
-
 module.exports = sequelize;
